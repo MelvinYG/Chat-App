4d8d1eeb4205ba529c9a4c0f7aa7f89fab8654c4
MelvinYG <melvingeorge204@gmail.com>
2024-06-10 12:56:58 +0530

    feat: Add user search and chatList updtates

--- src/components/list/chatList/chatItem/chatItem.jsx
+++ src/components/list/chatList/chatItem/chatItem.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable react/prop-types */
 import './chatItem.css'
 
-const ChatItem = () => {
+const ChatItem = ({chat}) => {
     return (
         <>
             <div className='chat-item'>
-                <img src="./avatar.jpg" alt="" />
+                <img src={chat.user.avatar || "./avatar.jpg"} alt="" />
                 <div className="texts">
-                    <span className='user-name'>Melvin George</span>
-                    <p>Hey there !!</p>
+                    <span className='user-name'>{chat.user.username}</span>
+                    <p>{chat.lastMessage}</p>
                 </div>
                 <span className= 'notification'>99+</span>
             </div>
--- src/components/list/chatList/chatList.jsx
+++ src/components/list/chatList/chatList.jsx
@@ -2,17 +2,29 @@ import { useEffect, useState } from 'react';
 import './chatList.css';
 import ChatItem from './chatItem/chatItem';
 import { useUserStore } from '../../../lib/useStore';
-import { doc, onSnapshot } from 'firebase/firestore';
+import { arrayUnion, collection, doc, getDoc, getDocs, onSnapshot, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore';
 import { database } from '../../../lib/firebase';
 
 const ChatList = () => {
 
-    const {currentUser} = useUserStore();
-    const [chats,setChats] = useState([]);
+    const { currentUser } = useUserStore();
+    const [chats, setChats] = useState([]);
 
     useEffect(() => {
-        const unsub = onSnapshot(doc(database, "userchats", currentUser.id), (doc) => {
-            setChats(doc.data());
+        const unsub = onSnapshot(doc(database, "userchats", currentUser.id), async (res) => {
+            const items = res.data().chats;
+
+            const promises = items.map(async (item) => {
+                const userDocRef = doc(database, "users", item.receiverId);
+                const userDocSnap = await getDoc(userDocRef);
+
+                const user = userDocSnap.data();
+
+                return { ...item, user };
+            });
+
+            const chatData = await Promise.all(promises);
+            setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt)); // to sort the recent chants first 
         });
 
         return () => {
@@ -20,11 +32,73 @@ const ChatList = () => {
         }
     }, [currentUser.id]);
 
+    // -------------------------- Above logic to update the userchat list -------------------------- //
+
+    // -------------------------- Below logic for adding user ----------------------- //
     const [addMode, setAddMobe] = useState(false);
 
-    function addHandler(){
+    function addHandler() {
         setAddMobe(prev => !prev);
     }
+
+    const [user, setUser] = useState(null);
+
+    const handleSearch = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const username = formData.get("username");
+    
+        try {
+            const userRef = collection(database, "users");
+            const q = query(userRef, where("username", "==", username));
+            const querySnapshot = await getDocs(q); // Correct function for queries
+    
+            if (!querySnapshot.empty) {
+                setUser(querySnapshot.docs[0].data()); // Assuming you only need the first match
+            }
+        } catch (err) {
+            console.error("Error searching user:", err);
+        }
+    }
+
+    // ------------------- Adding user to the chatlist -------------------------- //
+
+    const addUserHandle = async () => {
+        const chatRef = collection(database,"chats");
+        const userChatsRef = collection(database, "userchats");
+
+        try{
+            const newChatRef = doc(chatRef); // created a newchat with user and current user
+
+            await setDoc(newChatRef, {
+                createdAt: serverTimestamp(),
+                messages:[],
+            });
+            //  document update for user we are chatting with
+            await updateDoc(doc(userChatsRef, user.id), {
+                chats: arrayUnion({
+                    chatId: newChatRef.id,
+                    lastMessage: "",
+                    receiverId: currentUser.id,
+                    updatedAt: Date.now(), 
+                    // as we are already using a firebase method so cant use servertimestamp here
+                })
+            });
+
+            await updateDoc(doc(userChatsRef, currentUser.id), {
+                chats: arrayUnion({
+                    chatId: newChatRef.id,
+                    lastMessage: "",
+                    receiverId: user.id,
+                    updatedAt: Date.now(),
+                })
+            });
+
+        }catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <>
             <div className='chat-list'>
@@ -34,10 +108,29 @@ const ChatList = () => {
                         <input type="text" placeholder='Search' />
                     </div>
                     <img src={addMode ? "./minus.png" : "./plus.png"} onClick={addHandler} alt="" />
+                    {addMode ? (<div className="add-user-div">
+                        <form className="search-user" onSubmit={handleSearch}>
+                            <div className="search-field">
+                                <input type="text" placeholder='Username' name='username' />
+                            </div>
+                            <button className='btn primary'>Search</button>
+                        </form>
+                        {user && (<div className="user-searchlist">
+                            <div className="user-item">
+                                <div className="user-details">
+                                    <div className="img-container">
+                                        <img src={user.avatar ||"./avatar.jpg"} alt="" />
+                                    </div>
+                                    <span>{user.username}</span>
+                                </div>
+                                <button className='btn add-btn primary' onClick={addUserHandle}>Add User</button>
+                            </div>
+                        </div> )}
+                    </div>)  : <></>}
                 </div>
                 {/* List of recent chats */}
                 <div className="recent-chats">
-                    {chats.length>0 && chats.map((chat,index) => (
+                    {chats.length > 0 && chats.map((chat, index) => (
                         <ChatItem className="item" key={index} chat={chat}></ChatItem>
                     ))}
                 </div>
